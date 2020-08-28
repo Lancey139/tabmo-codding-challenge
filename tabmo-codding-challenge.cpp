b8fc66874776be5479e4735a6b3e9a4ee9d67c35
Nicolas MEO <nicolas.meo@gmail.com>
2020-08-28 19:48:21 +0200

    Création du projet CMake

---
+++ tabmo-codding-challenge.cpp
@@ -0,0 +1,12 @@
+﻿// tabmo-codding-challenge.cpp : définit le point d'entrée de l'application.
+//
+
+#include "tabmo-codding-challenge.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake test." << endl;
+	return 0;
+}
